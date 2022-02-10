7d7eba5546f5320c9794b88df2da625eb702d67b
Dariusz Gertych <dariusz.gertych@monterail.com>
2022-02-10 19:47:03 +0100

    Add console.log

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {
+  console.info("CONSOLE.INFO");
+  console.log("CONSOLE.LOG");
+  console.error("CONSOLE.ERROR");
+  console.debug("CONSOLE.debug");
+  res.type('html').send(html)
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
